13326fda794fcd03c742ed4fb519dedd93ebde7c
jon-is-learning <jonathan.garrett@gmail.com>
2016-08-18 12:10:42 -0700

    Complete bare minimums for promises micro sprint

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -8,13 +8,27 @@
  * HINT: We exported some similar promise-returning functions in previous exercises
  */
 
-var fs = require('fs');
 var Promise = require('bluebird');
-
+var fs = Promise.promisifyAll(require('fs'));
+var request = Promise.promisify(require('request'));
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return fs.readFileAsync(readFilePath)
+  .then(function(data) {
+    return data.toString().split('\n')[0];
+  }).then(function(username) {
+    var options = {
+      url: 'https://api.github.com/users/' + username,
+      headers: { 'User-Agent': 'request' },
+      json: true  // will JSON.parse(body) for us
+    };
+    return request(options)
+    .then(function(profile) {
+      // console.log(profile.body);
+      return fs.writeFileAsync(writeFilePath, JSON.stringify(profile.body));
+    });
+  });
 };
 
 // Export these functions so we can test them
